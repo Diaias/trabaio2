be0a18a53be35caca161693760a71bae8e5c55bc
LeonardoDr150 <leoprogramador150@gmail.com>
2023-12-03 12:55:08 -0300

    edit serviço

--- vue-route-cs/src/components/servico/Edit.vue
+++ vue-route-cs/src/components/servico/Edit.vue
@@ -1,132 +1,176 @@
 <template>
     <div id="tab_aut">
-     
-       <div v-if="currentEquipamento" class="edit-form">
-            <h3>Equipamento</h3>
+
+        <div v-if="currentServico" class="edit-form">
+            <h3>Servico</h3>
             <form>
                 <div class="form-group">
                     <label for="inputNome">Nome:</label>
-                    <input type="text" v-model="currentEquipamento.nome" class="form-control" id="inputNome">
-                </div>
-                <div class="form-group">
-                        <label for="inputMarca">Marca:</label>
-                        <input type="text" v-model="currentEquipamento.marca" class="form-control" id="inputMarca" >
+                    <input type="text" v-model="currentServico.nome" class="form-control" id="inputNome">
                 </div>
                 <div class="form-group">
                     <label for="inputDescricao">Descrição:</label>
-                    <input type="text" v-model="currentEquipamento.descricao" class="form-control" id="inputDescricao" >
+                    <input type="text" v-model="currentServico.descricao" class="form-control" id="inputDescricao">
                 </div>
                 <div class="form-group">
-                    <label for="selectPeca">Pecas:</label>
-                    <select v-model="currentEquipamento.pecas" class="form-control" id="selectPeca" multiple>                                
-                        <option v-for="p in pecas" :key ="p.id" v-bind:value="p">
-                            {{ p.nome }}
-                            </option>
-                    </select>
-                    
-                </div>                        
+                <label for="inputOrcamento">Orcamento:</label>
+                <input type="text" v-model="servico.orcamento" class="form-control" id="inputOrcamento">
+            </div>
+
+            <!--Select Cliente-->
+            <div class="form-group">
+                <label for="selectCliente">Clientes:</label>
+                <select v-model="servico.cliente" class="form-control" id="selectCliente" >
+                    <option v-for="c in cliente" :key="c.cpf" v-bind:value="c">
+                        {{ c.nome }}
+                    </option>
+                </select>
+            </div>
+
+            <!--Select Funcionario-->
+            <div class="form-group">
+                <label for="selectFuncionario">Funcionarios:</label>
+                <select v-model="servico.funcionario" class="form-control" id="selectFuncionario" >
+                    <option v-for="f in funcionario" :key="f.cpf" v-bind:value="f">
+                        {{ f.nome }}
+                    </option>
+                </select>
+            </div>
+
+            <!--Select Equipamento-->
+            <div class="form-group">
+                <label for="selectEquipamento">Equipamentos:</label>
+                <select v-model="servico.equipamento" class="form-control" id="selectEquipamento" multiple>
+                    <option v-for="e in equipamento" :key="e.id" v-bind:value="e">
+                        {{ e.nome }}
+                    </option>
+                </select>
+            </div>
             </form>
-            <button class="badge badge-success" @click="updateEquipamento">Salvar</button>
-            <button class="badge badge-danger mr-2" @click="deleteEquipamento">Delete</button>
+            <button class="badge badge-success" @click="updateServico">Salvar</button>
+            <button class="badge badge-danger mr-2" @click="deleteServico">Delete</button>
             <button class="badge badge-danger mr-2" @click="voltar">Voltar</button>
 
-            
+
             <p>{{ message }}</p>
 
-       </div>
-       <div v-else>
+        </div>
+        <div v-else>
             <br />
-            <p>Please click on a Equipamento...</p>
+            <p>Please click on a Servico...</p>
         </div>
 
-                                          
+
     </div>
- </template>
- <script>
- 
-     import EquipamentoDataService from '../../services/EquipamentoDataService'
-     import PecaDataService from '../../services/PecaDataService'
- 
-     export default{
-      name:'editEquipamentos',
-      data() {
-             return {                
-                 currentEquipamento: null,
-                 message: '',
-                 pecas: []
-             }
-         },
-         methods: {
+</template>
+<script>
 
-            getEquipamento(id){
+import ServicoDataService from '../../services/ServicoDataService'
+import ClienteDataService from '../../services/ClienteDataService'
+import FuncionarioDataService from '../../services/FuncionarioDataService'
+import EquipamentoDataService from '../../services/EquipamentoDataService'
 
-                EquipamentoDataService.get(id)
-                .then(response => {
-                    
-                    console.log(response.data);
-
-                    this.currentEquipamento = response.data;
-                    
-                })
-                .catch(e=> {
-                    console.log(e);
-                })
-            },
-            listPecas(){
-
-                PecaDataService.list().then(response =>{
-
-                    console.log("Retorno do seviço PecaDataService.list", response.status);
-                    console.log(response.data)
-                    this.pecas = response.data;                                  
-
-                }).catch(response => {
-
-                // error callback
-                alert('Não conectou no serviço PecaDataService.list');
-                console.log(response);
-                });               
-            },
-            updateEquipamento(){
-
-                EquipamentoDataService.update(this.currentEquipamento)
-                .then(response => {
-                    console.log('EquipamentoDataService.update');
-                    this.message = 'Equipamento alterado com sucesso !';
-                })
-                .catch(e =>{
-                    console.log(e);
-                })
-            },
-            deleteEquipamento(){
-
-                EquipamentoDataService.delete(this.currentEquipamento.id)
+export default {
+    name: 'editServicos',
+    data() {
+        return {
+            currentServico: null,
+            message: '',
+            cliente: [],
+            funcionario: [],
+            equipamento: []
+        }
+    },
+    methods: {
+        getServico(id) {
+            ServicoDataService.get(id)
                 .then(response => {
                     console.log(response.data);
-                    this.$router.push({ name: "equipamento-list" });
+                    this.currentServico = response.data;
                 })
                 .catch(e => {
-                console.log(e);
-                });
-            },
-            voltar(){
-                this.$router.push({ name: "equipamento-list" });
-            }
-         },
-         mounted() {
-            
-            this.message = '';
-            this.listPecas();
-            this.getEquipamento(this.$route.params.id);
-         }
-     }
- </script>
-    
- <style scoped>
+                    console.log(e);
+                })
+        },
 
-    .edit-form {
+        //List cliente
+        listCliente() {
+            ClienteDataService.list().then(response => {
+                console.log("Retorno do seviço ClienteDataService.list", response.status);
+                for (let e of response.data) {
+                    this.cliente.push(e);
+                }
+            }).catch(response => {
+                // error callback
+                alert('Não conectou no serviço ClienteDataService.list');
+                console.log(response);
+            });
+        },
+        //List funcionario
+        listFuncionario() {
+            FuncionarioDataService.list().then(response => {
+                console.log("Retorno do seviço FuncionarioDataService.list", response.status);
+                for (let e of response.data) {
+                    this.funcionario.push(e);
+                }
+            }).catch(response => {
+                // error callback
+                alert('Não conectou no serviço FuncionarioDataService.list');
+                console.log(response);
+            });
+        },
+        //List equipamento
+        listEquipamento() {
+            EquipamentoDataService.list().then(response => {
+                console.log("Retorno do seviço EquipamentoDataService.list", response.status);
+                for (let e of response.data) {
+                    this.equipamento.push(e);
+                }
+            }).catch(response => {
+                // error callback
+                alert('Não conectou no serviço EquipamentoDataService.list');
+                console.log(response);
+            });
+        },
+
+        updateServico() {
+
+            ServicoDataService.update(this.currentServico)
+                .then(response => {
+                    console.log('ServicoDataService.update');
+                    this.message = 'Servico alterado com sucesso !';
+                })
+                .catch(e => {
+                    console.log(e);
+                })
+        },
+        deleteServico() {
+
+            ServicoDataService.delete(this.currentServico.id)
+                .then(response => {
+                    console.log(response.data);
+                    this.$router.push({ name: "servico-list" });
+                })
+                .catch(e => {
+                    console.log(e);
+                });
+        },
+        voltar() {
+            this.$router.push({ name: "servico-list" });
+        }
+    },
+    mounted() {
+
+        this.message = '';
+        this.listPecas();
+        this.getServico(this.$route.params.id);
+    }
+}
+</script>
+    
+<style scoped>
+.edit-form {
     max-width: 300px;
     margin: auto;
-    }
-     
- </style>
+}
+</style>
